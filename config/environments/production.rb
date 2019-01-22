7a6ffa87018ca85aa1be1ae49193f5f454afe4bc
Evgeniy Kovyryashkin <seventhstar@mail.ru>
2019-01-22 19:04:43 +0300

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,24 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.host = 'pasca1905.herokuapp.com'
+  host = 'pasca1905.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
+
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
